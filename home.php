e9b74fd7ab8b527cb77eac1b923cd5510a7fcf56
elbayoumi <mohamedashrafelbayoumi@gmail.com>
2023-10-27 17:34:37 +0200

    first

---
+++ home.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/index.css">
+
+    <title>Home</title>
+</head>
+
+<body>
+    <div class="container">
+
+        <header>
+            <img src="./assets/img/1679602564028.jpeg" alt="Logo" class="logo">
+            <!-- Add navigation links if needed -->
+        </header>
+
+        <div class="slider-container">
+            <div class="slider">
+
+                 <img src="https://www.w3schools.com/tags/img_girl.jpg" alt="Slide 1">
+                 <img src="https://www.w3schools.com/tags/img_girl.jpg" alt="Slide 2">
+                 <img src="https://www.w3schools.com/tags/img_girl.jpg" alt="Slide 3">
+                 <img src="https://www.w3schools.com/tags/img_girl.jpg" alt="Slide 4">
+                 <img src="https://www.w3schools.com/tags/img_girl.jpg" alt="Slide 5">
+                 <img src="https://www.w3schools.com/tags/img_girl.jpg" alt="Slide 6">
+                 <img src="https://www.w3schools.com/tags/img_girl.jpg" alt="Slide 7">
+                 <img src="https://www.w3schools.com/tags/img_girl.jpg" alt="Slide 8">
+                 <img src="https://www.w3schools.com/tags/img_girl.jpg" alt="Slide 9">
+
+            </div>
+            <button class="prev-btn">Previous</button>
+            <button class="next-btn">Next</button>
+        </div>
+
+        <main class="main-content">
+            <section>
+                <h2>Section 3</h2>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum amet pariatur rerum corrupti obcaecati suscipit repellat quod aspernatur error dicta neque, saepe deserunt quibusdam esse magnam sit eum laudantium illum? </p>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum amet pariatur rerum corrupti obcaecati suscipit repellat quod aspernatur error dicta neque, saepe deserunt quibusdam esse magnam sit eum laudantium illum? </p>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum amet pariatur rerum corrupti obcaecati suscipit repellat quod aspernatur error dicta neque, saepe deserunt quibusdam esse magnam sit eum laudantium illum? </p>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum amet pariatur rerum corrupti obcaecati suscipit repellat quod aspernatur error dicta neque, saepe deserunt quibusdam esse magnam sit eum laudantium illum? </p>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum amet pariatur rerum corrupti obcaecati suscipit repellat quod aspernatur error dicta neque, saepe deserunt quibusdam esse magnam sit eum laudantium illum? </p>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum amet pariatur rerum corrupti obcaecati suscipit repellat quod aspernatur error dicta neque, saepe deserunt quibusdam esse magnam sit eum laudantium illum? </p>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum amet pariatur rerum corrupti obcaecati suscipit repellat quod aspernatur error dicta neque, saepe deserunt quibusdam esse magnam sit eum laudantium illum? </p>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum amet pariatur rerum corrupti obcaecati suscipit repellat quod aspernatur error dicta neque, saepe deserunt quibusdam esse magnam sit eum laudantium illum? </p>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum amet pariatur rerum corrupti obcaecati suscipit repellat quod aspernatur error dicta neque, saepe deserunt quibusdam esse magnam sit eum laudantium illum? </p>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum amet pariatur rerum corrupti obcaecati suscipit repellat quod aspernatur error dicta neque, saepe deserunt quibusdam esse magnam sit eum laudantium illum? </p>
+            </section>
+        </main>
+        <footer>
+            <p>&copy; 2023 task job. All rights reserved.</p>
+        </footer>
+    </div>
+
+</body>
+<script>
+
+</script>
+<script src="./assets/js/index.js"></script>
+
+</html>
--- index.php
+++ index.php
@@ -4,64 +4,38 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/css/index.css">
+    <style>
+        *{
+            margin: 0;
+            padding: 0;
 
-    <title>Home</title>
+        }
+        body{
+            
+        }
+        form{
+            
+            margin: auto;
+            width: 50%;
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+            align-items: center;
+        }
+        form>*{
+            /* display: block; */
+        }
+    </style>
+    <title>login</title>
 </head>
 
 <body>
-    <div class="container">
-
-        <header>
-            <img src="./assets/img/1679602564028.jpeg" alt="Logo" class="logo">
-            <!-- Add navigation links if needed -->
-        </header>
-
-        <div class="slider-container">
-            <div class="slider">
-
-                 <img src="https://www.w3schools.com/tags/img_girl.jpg" alt="Slide 1">
-                 <img src="https://www.w3schools.com/tags/img_girl.jpg" alt="Slide 2">
-                 <img src="https://www.w3schools.com/tags/img_girl.jpg" alt="Slide 3">
-                 <img src="https://www.w3schools.com/tags/img_girl.jpg" alt="Slide 4">
-                 <img src="https://www.w3schools.com/tags/img_girl.jpg" alt="Slide 5">
-                 <img src="https://www.w3schools.com/tags/img_girl.jpg" alt="Slide 6">
-                 <img src="https://www.w3schools.com/tags/img_girl.jpg" alt="Slide 7">
-                 <img src="https://www.w3schools.com/tags/img_girl.jpg" alt="Slide 8">
-                 <img src="https://www.w3schools.com/tags/img_girl.jpg" alt="Slide 9">
-
-            </div>
-            <button class="prev-btn">Previous</button>
-            <button class="next-btn">Next</button>
-        </div>
-
-        <main class="main-content">
-            <section>
-                <h2>Section 3</h2>
-                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum amet pariatur rerum corrupti obcaecati suscipit repellat quod aspernatur error dicta neque, saepe deserunt quibusdam esse magnam sit eum laudantium illum? </p>
-                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum amet pariatur rerum corrupti obcaecati suscipit repellat quod aspernatur error dicta neque, saepe deserunt quibusdam esse magnam sit eum laudantium illum? </p>
-                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum amet pariatur rerum corrupti obcaecati suscipit repellat quod aspernatur error dicta neque, saepe deserunt quibusdam esse magnam sit eum laudantium illum? </p>
-                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum amet pariatur rerum corrupti obcaecati suscipit repellat quod aspernatur error dicta neque, saepe deserunt quibusdam esse magnam sit eum laudantium illum? </p>
-                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum amet pariatur rerum corrupti obcaecati suscipit repellat quod aspernatur error dicta neque, saepe deserunt quibusdam esse magnam sit eum laudantium illum? </p>
-                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum amet pariatur rerum corrupti obcaecati suscipit repellat quod aspernatur error dicta neque, saepe deserunt quibusdam esse magnam sit eum laudantium illum? </p>
-                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum amet pariatur rerum corrupti obcaecati suscipit repellat quod aspernatur error dicta neque, saepe deserunt quibusdam esse magnam sit eum laudantium illum? </p>
-                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum amet pariatur rerum corrupti obcaecati suscipit repellat quod aspernatur error dicta neque, saepe deserunt quibusdam esse magnam sit eum laudantium illum? </p>
-                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum amet pariatur rerum corrupti obcaecati suscipit repellat quod aspernatur error dicta neque, saepe deserunt quibusdam esse magnam sit eum laudantium illum? </p>
-                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum amet pariatur rerum corrupti obcaecati suscipit repellat quod aspernatur error dicta neque, saepe deserunt quibusdam esse magnam sit eum laudantium illum? </p>
-            </section>
-
-            <!-- Repeat the section structure for Sections 2, 3, and 4 -->
-
-        </main>
-        <footer>
-            <p>&copy; 2023 task job. All rights reserved.</p>
-        </footer>
-    </div>
-
+    <form action="">
+        <label for="username">username</label>
+        <input type="text" name="username" id="username">
+        <label for="password">password</label>
+        <input type="text" name="password" id="password">
+    </form>
 </body>
-<script>
-
-</script>
-<script src="./assets/js/index.js"></script>
 
 </html>
